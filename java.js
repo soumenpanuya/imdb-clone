8e27b7ced3c823f1d9da3f75d09a01bc4af786a9
soumen panuya <soumenpanuya86@gmail.com>
2023-06-19 22:36:51 +0530

    search add

--- java.js
+++ java.js
@@ -1,10 +1,12 @@
 const input = document.getElementById('input_field');
-const post_container =document.getElementById('post_container');
+const post_container =document.getElementById('poster_container');
+const search = document.getElementById("search_icon");
 
 // pre search poster id.....
 
 const arr=['tt5971474',"tt3896198","tt12915716","tt5090568","tt1630029","tt4154796","tt10872600","tt13353462","tt1345836","tt10838180","tt0910970","tt1201607"];
 
+// show movie using id....
  function fetchdata(id){
 
      fetch(`https://www.omdbapi.com/?i=${id}&apikey=ad538ff1`).then((res)=>{
@@ -27,10 +29,51 @@ const arr=['tt5971474',"tt3896198","tt12915716","tt5090568","tt1630029","tt41547
             
                 poster_container.appendChild(after);
 
+    }).catch((err)=>{
+        console.log(err);
     })
 }
 
+// search movie id function...
+
+function searchid(name){
+    fetch(`https://www.omdbapi.com/?s=${input.value}&apikey=ad538ff1`).then((res)=>{
+        return res.json();
+    }).then((result)=>{
+        post_container.innerHTML="";
+        console.log(result.Search[0].imdbID);
+        fetchdata(result.Search[0].imdbID);
+        
+    }).catch((err)=>{
+        console.log("error come..")
+        console.log(err);
+
+    })
+}
+
+// search event....
+search.onclick=()=>{
+    if(input.value!="")
+    {
+     searchid(input.value);
+    }
+}
+document.onkeyup= (e)=>{
+    if(e.key=="Enter")
+    {
+        searchid(input.value);
+    }
+}
+
+input.onkeyup=(e)=>{
+    if(input.value=='')
+    {
+        render();
+    }
+}
+
  function render(){
+    post_container.innerHTML="";
     arr.forEach((id)=>{
         fetchdata(id);
     })
