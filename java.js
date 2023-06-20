70cf657b945a28d921da8dc980da02e291060eca
soumen panuya <soumenpanuya86@gmail.com>
2023-06-20 11:11:16 +0530

    favourite added

--- java.js
+++ java.js
@@ -1,15 +1,19 @@
 const input = document.getElementById('input_field');
 const post_container =document.getElementById('poster_container');
 const search = document.getElementById("search_icon");
+const favourite_list = document.getElementById("watchlist");
 
 // pre search poster id.....
 
 const arr=['tt5971474',"tt3896198","tt12915716","tt5090568","tt1630029","tt4154796","tt10872600","tt13353462","tt1345836","tt10838180","tt0910970","tt1201607"];
 
-// show movie using id....
- function fetchdata(id){
+// favourite list ....
+let fav =[];
 
-     fetch(`https://www.omdbapi.com/?i=${id}&apikey=ad538ff1`).then((res)=>{
+// show movie using id....
+ async function fetchdata(id){
+
+    await fetch(`https://www.omdbapi.com/?i=${id}&apikey=ad538ff1`).then((res)=>{
         return res.json();
     }).then((result)=>{
         let after =document.createElement("div");
@@ -25,7 +29,7 @@ const arr=['tt5971474',"tt3896198","tt12915716","tt5090568","tt1630029","tt41547
                     <i class="fa-regular fa-star"></i>
                 </div>
                 <div class="title">${result.Title}</div>
-                <div class="favourite">+ Favourite</div>`;
+                <div id ="${id}" class="favourite" onclick="favourite_add(this)">+ Favourite</div>`;
             
                 poster_container.appendChild(after);
 
@@ -34,6 +38,17 @@ const arr=['tt5971474',"tt3896198","tt12915716","tt5090568","tt1630029","tt41547
     })
 }
 
+// render movie list....
+function render(arr){
+    post_container.innerHTML="";
+    arr.forEach((id)=>{
+        fetchdata(id);
+    })
+ }
+
+render(arr);
+
+
 // search movie id function...
 
 function searchid(name){
@@ -41,14 +56,34 @@ function searchid(name){
         return res.json();
     }).then((result)=>{
         post_container.innerHTML="";
-        console.log(result.Search[0].imdbID);
-        fetchdata(result.Search[0].imdbID);
+        let length=result.Search.length;
+
+        for( let i=0;i<length;i++)
+            {
+                fetchdata(result.Search[i].imdbID);
+            }
         
     }).catch((err)=>{
         console.log("error come..")
         console.log(err);
+        post_container.innerHTML="<h1>Result not found</h1>"
 
     })
+}
+
+function favourite_add(element){
+    let present=false;
+    fav.forEach((id)=>{
+        if(id==element.id)
+        {
+            alert("already added this movie...");
+            present=true;
+            return;
+        }
+    })
+    if(!present){
+        fav.push(element.id);
+    }
 }
 
 // search event....
@@ -61,22 +96,13 @@ search.onclick=()=>{
 document.onkeyup= (e)=>{
     if(e.key=="Enter")
     {
-        searchid(input.value);
+        if(input.value!=""){
+
+            searchid(input.value);
+        }
     }
 }
 
-input.onkeyup=(e)=>{
-    if(input.value=='')
-    {
-        render();
-    }
+favourite_list.onclick=()=>{
+    render(fav);
 }
-
- function render(){
-    post_container.innerHTML="";
-    arr.forEach((id)=>{
-        fetchdata(id);
-    })
- }
-
-render();
